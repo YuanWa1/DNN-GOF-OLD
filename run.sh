0ba5c83041fb01c60980904ba05ffe29d3f6938d
Xiaoming Wang <x_w21@login2.cm.cluster>
2024-09-18 10:44:58 -0500

    update sh file

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #SBATCH -J test
 #SBATCH -p shared
 #SBATCH -N 1 -c 4
-#SBATCH --time=00:30:00
+#SBATCH --time=02:00:00
 #SBATCH --mem=1024M
 
 #SBATCH --array=1-1000
@@ -10,6 +10,5 @@
 module load python39
 module load cuda11.0/toolkit/11.0.3
 source myenv/bin/activate
-python3 power_test_4.py $1
-python3 power_test_5_cos.py $1
+python3 test.py $1
 
