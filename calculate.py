37b1f78bf59fed19c3f5d99057b2bc4515610b14
Xiaoming Wang <x_w21@login2.cm.cluster>
2024-09-18 10:21:11 -0500

    update calculate.py and final result

--- calculate.py
+++ calculate.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np 
 import sys
+import os
 
 def check(column_name,df):
     df[column_name + '_check'] = np.where(df[column_name] < 0.05, 'TRUE', 'FALSE')
@@ -12,7 +13,7 @@ def count(column_name,df,final_count):
 def main():
     print(f"Processing file: {sys.argv[1]}")
 
-    df = pd.read_csv(sys.argv[1],skiprows=1)
+    df = pd.read_csv(sys.argv[1],skiprows=2)
 
     column_names = df.columns
     final_count = {}
@@ -21,14 +22,19 @@ def main():
 
     for column in column_names:
         count(column,df=df,final_count=final_count)
-
-    df = df._append(final_count, ignore_index=True)
-
-    print(df)
-    loc = df.columns.get_loc('p_lm_check')
-    df.insert(loc, '', np.nan)
-    new_filename = sys.argv[1].split('.')[0] + '_updated.csv'
-    df.to_csv(new_filename, index=False, na_rep='')
+    
+    # df = df._append(final_count, ignore_index=True)
+    # print(df)
+    # loc = df.columns.get_loc('p_lm_check')
+    # df.insert(loc, '', np.nan)
+    # new_filename = sys.argv[1].split('.')[0] + '_updated.csv'
+    # df.to_csv(new_filename, index=False, na_rep='')
+    if not os.path.exists('final_result.txt'):
+        f = open("final_result.txt",'x')
+    f = open("final_result.txt",'a')
+    f.write(sys.argv[1] + ':\n')
+    f.write(str(final_count)+'\n\n')
+    f.close()
     
     
 if __name__ == "__main__":
